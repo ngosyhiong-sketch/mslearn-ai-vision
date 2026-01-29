25430ed9d5dc2850e9b1e47f15ab25865908795b
Sy Hiong Ngo <sy.hiong.ngo@kyndryl.com>
2026-01-29 14:20:57 +0100

    import namespaces

--- Labfiles/analyze-images/python/image-analysis/image-analysis.py
+++ Labfiles/analyze-images/python/image-analysis/image-analysis.py
@@ -7,7 +7,10 @@ from azure.core.exceptions import HttpResponseError
 import requests
 
 # import namespaces
-
+# import namespaces
+from azure.ai.vision.imageanalysis import ImageAnalysisClient
+from azure.ai.vision.imageanalysis.models import VisualFeatures
+from azure.core.credentials import AzureKeyCredential
 
 def main():
 
